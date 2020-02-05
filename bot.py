799c048b9764a95f82cb36c1b49c757abcbc6cc7
YuHuan Hsu <yeuhwan.hsu@gmail.com>
2020-02-06 03:56:24 +0800

    Audio Init

--- bot.py
+++ bot.py
@@ -4,32 +4,17 @@ import asyncio
 import datetime
 import pytz
 from discord.ext import commands
-
-messages = joined = 0
-
+import os
+from discord.utils import get
+import youtube_dl
 
 token = 'Njc0MzQ1NDg2MDY4NDgyMDUx.XjnUyA.UKpJP_i1g9kQGTzbB5jFDagoqRI'
-
 client = commands.Bot(command_prefix = '!')
 timechannel = 674348205948796958
 memberchannel = 674348439856742430
+guild = 569414055438319637
 tz = pytz.timezone('Asia/Shanghai')
-'''async def update_stats():
-    await client.wait_until_ready()
-    global messages, joined
 
-    while not client.is_closed():
-        try:
-            with open("stats.txt", "a") as f:
-                f.write(f"Time: {int(time.time())}, Messages: {messages}, Members Joined: {joined}\n")
-
-            messages = 0
-            joined = 0
-
-            await asyncio.sleep(60)
-        except Exception as e:
-            print(e)
-            await asyncio.sleep(60)'''
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -38,45 +23,110 @@ async def on_ready():
         times = "上午"
         hour = now.hour
         minute = now.minute
-        if(hour > 12):
-            hour -= 12
+        if(hour >= 12):
+            if hour != 12:
+                hour -= 12
             times = "下午"
         await client.get_channel(timechannel).edit(name=f"台灣{times}{hour}時{minute}分 ") # The channel gets changed here
         await asyncio.sleep(10)
 @client.event
 async def on_member_join(member):
-    id = client.get_guild(569414055438319637)
-    global joined
-    joined += 1
+    id = client.get_guild(guild)
     for channel in member.guild.channels:
         if str(channel) == "join":
             await channel.send(f"""Welcome to the server {member.mention}""")
     await client.get_channel(memberchannel).edit(name=f"伺服器人數\t {id.member_count} ")
-@client.event
-async def on_message(message):
-    global messages
-    messages += 1
-    id = client.get_guild(569414055438319637)
-    if message.content.find("!hello") != -1:
-        await message.channel.send("怎麼啦~主人?")
-    elif message.content == "!users":
-        await message.channel.send(f"""# of Members: {id.member_count} """)
-    elif message.content == "!confess":
-        await message.channel.send("我的心意還沒有傳達給你")
-        time.sleep(2)
-        await message.channel.send("所以")
-        time.sleep(2)
-        await message.channel.send("我希望")
-        time.sleep(2)
-        await message.channel.send("你能多了解我一些")
-        time.sleep(2)
-        await message.channel.send("我想要讓你知道")
-        time.sleep(2)
-        await message.channel.send("我是有多麼喜歡你")
-        time.sleep(2)
-        await message.channel.send("做好覺悟吧!")
-        time.sleep(2)
-        await message.channel.send("主人<3")
-        await message.channel.send("https://imgur.com/EOPfIbh")
-'''client.loop.create_task(update_stats())'''
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("怎麼啦~主人?")
+@client.command()
+async def users(ctx):
+    id = client.get_guild(guild)
+    await ctx.send(f"""Num of Members : {id.member_count} """)
+@client.command()
+async def confess(ctx):
+    await ctx.send("我的心意還沒有傳達給你")
+    time.sleep(2)
+    await ctx.send("所以")
+    time.sleep(2)
+    await ctx.send("我希望")
+    time.sleep(2)
+    await ctx.send("你能多了解我一些")
+    time.sleep(2)
+    await ctx.send("我想要讓你知道")
+    time.sleep(2)
+    await ctx.send("我是有多麼喜歡你")
+    time.sleep(2)
+    await ctx.send("做好覺悟吧!")
+    time.sleep(2)
+    await ctx.send("主人<3")
+    time.sleep(1)
+    await ctx.send("https://imgur.com/EOPfIbh")
+@client.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"Left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("Don't think I am in a voice channel")
+
+@client.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.07
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
 client.run(token)
